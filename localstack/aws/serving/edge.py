18c530266651107dd4c4da09b125076aa3820c98
Thomas Rausch <thomas@thrau.at>
2022-12-28 01:42:16 +0100

    basic implementation of botocore modifications for in-memory request dispatching

--- localstack/aws/serving/edge.py
+++ localstack/aws/serving/edge.py
@@ -1,6 +1,6 @@
 from localstack.http.hypercorn import GatewayServer
+from localstack.runtime import components
 from localstack.runtime.shutdown import SHUTDOWN_HANDLERS
-from localstack.services.plugins import SERVICE_PLUGINS
 
 
 def serve_gateway(bind_address, port, use_ssl, asynchronous=False):
@@ -8,12 +8,8 @@ def serve_gateway(bind_address, port, use_ssl, asynchronous=False):
     Implementation of the edge.do_start_edge_proxy interface to start a Hypercorn server instance serving the
     LocalstackAwsGateway.
     """
-    from localstack.aws.app import LocalstackAwsGateway
-
-    gateway = LocalstackAwsGateway(SERVICE_PLUGINS)
-
     # start serving gateway
-    server = GatewayServer(gateway, port, bind_address, use_ssl)
+    server = GatewayServer(components.gateway(), port, bind_address, use_ssl)
     server.start()
 
     # with the current way the infrastructure is started, this is the easiest way to shut down the server correctly
